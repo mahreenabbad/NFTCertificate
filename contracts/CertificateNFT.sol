2d14ab5ff42232572f302ac023b3f9ecef1898c8
Mahreen <mahreenabbad@gmail.com>
2024-10-31 12:34:06 +0500

    Add changes to Smart Contract

--- contracts/CertificateNFT.sol
+++ contracts/CertificateNFT.sol
@@ -4,42 +4,56 @@ pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+// use soulBound 
+//check uri format from sepolia etherscan
+// correct uri format
+contract CertificateNFT is  ERC721, ERC721URIStorage, Ownable{
+   
 
-contract CertificateNFT is ERC721, ERC721URIStorage{
-
-    address public initialOwner;
-    uint public _nextTokenId;
-
-    mapping(uint=> bool) public approvedNFT;
-    mapping(uint => address) public boundNFTs;
+    uint public _nextTokenId=0;
     
-    //  mapping(uint => bool) public boundNFTRequests;
+    constructor(address initialOwner) ERC721("Student Certificate", "SCT")  Ownable(initialOwner) {  }
+
+   
+      modifier onlyApprovedSoulBound(uint _tokenId){
+            require(approveSoulBoundNFT[_tokenId] == false,"Not approved");
+            require(boundNFTMinted[_tokenId][msg.sender]== false,"Not approved");
+            _;
+   }
+
+     mapping(uint=> bool) public approveSoulBoundNFT;
+     mapping(uint =>mapping(address=>bool)) public boundNFTMinted;
+    
+   
     event BoundTokenMinted(address indexed to, uint256 indexed tokenId);
-    
-    constructor() ERC721("Student Certificate", "SCT")   {
-                initialOwner = msg.sender;}
+//mapping ----
+//modifier --- {
+   
+     function mintSoulBoundToken(address _to, string memory uri) public onlyOwner {
+        require(_to != address(0), "Invalid Address");
+        require(approveSoulBoundNFT[_nextTokenId] ==true, "NFT not approved");
+        require(boundNFTMinted[_nextTokenId][_to] ==false, "NFT not approved");
+        _safeMint(_to, _nextTokenId);
+        _setTokenURI(_nextTokenId, uri);
+        boundNFTMinted[_nextTokenId][_to]= true;
+        approveSoulBoundNFT[_nextTokenId] = false; // approval ended after minting
+        _nextTokenId ++;
+         emit BoundTokenMinted(_to, _nextTokenId);
+    }
 
-     modifier onlyOwner() {
-        require(msg.sender==initialOwner,"You are Not Owner");
-        _;
+     function approveSoulboundNFT( uint256 tokenId) external  onlyOwner {  
+        approveSoulBoundNFT[tokenId] = true;
     }
-     
-
-     function mintSoulBoundToken(address _to, uint _tokenId, string memory uri) external onlyOwner {
-        require(approvedNFT[_tokenId], "NFT not approved");
-      
-        _safeMint(_to, _tokenId);
-        _setTokenURI(_tokenId, uri);
-        boundNFTs[_tokenId] = msg.sender;
-         emit BoundTokenMinted(_to, _tokenId);
+        function transferFrom(address from, address to, uint256 tokenId) public  virtual onlyApprovedSoulBound(tokenId) override(ERC721, IERC721) {
+        super.transferFrom(from, to, tokenId);
     }
-      
-     function approveBoundNFT(uint _tokenId) external onlyOwner {
-        // require(boundNFTRequests[_tokenId], "Bound NFT  request not submitted");
-        approvedNFT[_tokenId] = true;
-    }
-   ////////////////////////////////////////////////
+  
+      function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public  virtual override(ERC721,IERC721)  {
+        transferFrom(from, to, tokenId);
+        ERC721Utils.checkOnERC721Received(_msgSender(), from, to, tokenId, data);
+      }
    
 
     // The following functions are overrides required by Solidity.
@@ -52,7 +66,7 @@ contract CertificateNFT is ERC721, ERC721URIStorage{
     {
         return super.tokenURI(tokenId);
     }
-    
+
 
     function supportsInterface(bytes4 interfaceId)
         public
@@ -61,16 +75,6 @@ contract CertificateNFT is ERC721, ERC721URIStorage{
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
-    }
-    /////////////////////
-     function _update(address to, uint256 tokenId, address auth) internal override returns (address) {
-        address from = _ownerOf(tokenId);
-        require(from == address(0) || to == address(0), "SoulBoundNFT: token transfer is not allowed");
-        require(approvedNFT[tokenId] ==true,"Bound NFT not Approved");
-        return super._update(to, tokenId, auth);
-    }
-    
-    
- 
-     
+    }       
 }
+///////////////////////////////
